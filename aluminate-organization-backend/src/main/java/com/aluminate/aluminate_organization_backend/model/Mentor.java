a8282d30323ac9592e740250c463b6d71aa925f6
sheanemario <sheanemario7@gmail.com>
2025-07-12 16:19:52 +0530

    Designed and implement the Mentor model ensuring foreign keys

---
+++ aluminate-organization-backend/src/main/java/com/aluminate/aluminate_organization_backend/model/Mentor.java
@@ -0,0 +1,43 @@
+package com.aluminate.aluminate_organization_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+
+public class Mentor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", referencedColumnName = "id")
+    private Member member;
+
+    private String availability;
+    private int yearsOfExperience;
+    private BigDecimal hourlyRate;
+    private String bio;
+    private String linkedInUrl;
+    private String portfolioUrl;
+    private String motivation;
+
+    @ElementCollection
+    @CollectionTable(name = "mentor_languages", joinColumns = @JoinColumn(name = "mentor_id"))
+    @Column(name = "language")
+    private Set<String> languages = new HashSet<>();
+
+    private String preferredMenteeLevel;
+    private int maxMentees;
+    private Double rating;
+    private int sessionCount;
+}
