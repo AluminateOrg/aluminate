ce8d68b2fa0570d6c267e3e7ae4538e9e485f0ec
sheanemario <sheanemario7@gmail.com>
2025-07-13 00:52:03 +0530

    Implement model to Store organization settings

---
+++ aluminate-organization-backend/src/main/java/com/aluminate/aluminate_organization_backend/model/OrganizationSettings.java
@@ -0,0 +1,30 @@
+package com.aluminate.aluminate_organization_backend.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@EqualsAndHashCode(of = {"id"})
+
+public class OrganizationSettings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String portalUrl;
+    private String organizationEmail;
+    private String phoneNumber;
+    private String address;
+    private String logoUrl;
+}
