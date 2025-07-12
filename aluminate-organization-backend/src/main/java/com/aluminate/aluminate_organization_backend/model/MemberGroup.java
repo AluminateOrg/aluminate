4ecc3bc88398b960fa7072ef1f880832bbd81d25
sheanemario <sheanemario7@gmail.com>
2025-07-12 18:14:28 +0530

    Implemented MemberGroup model to store membership details with referencial integrity

---
+++ aluminate-organization-backend/src/main/java/com/aluminate/aluminate_organization_backend/model/MemberGroup.java
@@ -0,0 +1,27 @@
+package com.aluminate.aluminate_organization_backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class MemberGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
+
+    private boolean isApproved; // true if the member has been approved to join the group
+    private String role; // "ADMIN", "MEMBER"
+}
