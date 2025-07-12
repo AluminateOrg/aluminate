78a16f5f68714ca7702b265cf5b305f7f73a33a1
sheanemario <sheanemario7@gmail.com>
2025-07-12 19:37:34 +0530

    Implement Campaign model

---
+++ aluminate-organization-backend/src/main/java/com/aluminate/aluminate_organization_backend/model/Campaign.java
@@ -0,0 +1,34 @@
+package com.aluminate.aluminate_organization_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@EqualsAndHashCode(of = {"id"})
+
+public class Campaign {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal target;
+    private String Category;
+    private BigDecimal currentAmount;
+    private int totalDonors;
+    private boolean isActive;
+
+}
