4156bc48421ba1221d113d6d5c94823b6be9578c
Satheera Jayawardhana <nirmalsatheera22@gmail.com>
2025-08-03 19:18:15 +0530

    admin fundraising part

--- aluminate-organization-backend/src/main/java/com/aluminate/aluminate_organization_backend/model/Campaign.java
+++ aluminate-organization-backend/src/main/java/com/aluminate/aluminate_organization_backend/model/Campaign.java
@@ -1,10 +1,12 @@
 package com.aluminate.aluminate_organization_backend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,34 +17,43 @@ import java.util.Set;
 @Entity
 @Builder
 @EqualsAndHashCode(of = {"id"})
-
 public class Campaign {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
     private String description;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private BigDecimal target;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, name = "type")
     private CampaignType type;
 
-    private BigDecimal currentAmount;
-    private int totalDonors;
-    private boolean isActive;
+    @Column(name = "target")
+    private BigDecimal goal;
+
+    @Column(name = "current_amount")
+    @Builder.Default
+    private BigDecimal raised = BigDecimal.ZERO;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "total_donors")
+    @Builder.Default
+    private int donorCount = 0;
+
+    @Column(name = "is_active")
+    @Builder.Default
+    private boolean isActive = true;
+
+    @Builder.Default
+    private boolean isDeleted = false;
+    private LocalDateTime deletedAt;
 
     @OneToMany(mappedBy = "campaign")
     private Set<Donation> donations = new HashSet<>();
-
-    /** Utility method to update currentAmount and totalDonors */
-    public void addDonation(Donation donation) {
-        donations.add(donation);
-        this.currentAmount = this.currentAmount.add(donation.getAmount());
-        this.totalDonors = donations.size();
-    }
-
-
 }
